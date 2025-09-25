4400cd65de2570d853cdcbaee7e7d485d2a6bf3d
shadowmoonezios <agushifelde@hotmail.com>
2025-09-25 17:34:29 +0300

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost/sprach-lerner', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB verbunden'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Willkommen zur Sprach Lerner App!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
